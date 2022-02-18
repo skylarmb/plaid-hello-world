54ef76086eb6b719b8a45394cd5a2c5687617ed1
ghernandez-plaid <86379616+ghernandez-plaid@users.noreply.github.com>
2022-02-18 17:32:07 -0600

    Consolidate code

--- src/App.jsx
+++ src/App.jsx
@@ -11,24 +11,37 @@ function App() {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  const createToken = useCallback(async () => {
-    const linkToken = await api.createLinkToken();
+  const createLinkToken = useCallback(async () => {
+    const response = await fetch ("/api/create_link_token", {
+      method: "GET",
+    });
+    const data = await response.json();
+    const linkToken = data.link_token;
     setToken(linkToken);
     setLoading(false);
   }, []);
 
   const onSuccess = useCallback(async (publicToken) => {
     setLoading(true);
-    await api.exchangePublicToken(publicToken);
-    const data = await api.getTransactions();
+    await fetch("/api/exchange_public_token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ public_token: publicToken }),
+    });
+    const response = await fetch("/api/data", {
+      method: "GET",
+    });
+    const data = await response.json();
     setData(data);
     setLoading(false);
   }, []);
 
   // generate link token on app load
   React.useEffect(() => {
-    createToken();
-  }, [createToken]);
+    createLinkToken();
+  }, [createLinkToken]);
 
   const { open, ready } = usePlaidLink({
     token,
